cc57c547eeede3985d4e737d1f54770b9d881581
Mike Thornton <361145+six8@users.noreply.github.com>
2023-05-19 00:39:01 -0500

    Added logos (#1)

---
+++ .build/convert.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# svgo required: pnpm -g install svgo
+# https://github.com/svg/svgo
+
+# Directory to output the .puml files
+output_dir="./plantuml"
+svg_archive="https://github.com/dev-details/gilbarbara-logos/archive/refs/heads/main.tar.gz"
+
+mkdir -p "$output_dir"
+
+# Download and extract the repository to a temporary directory
+tmp_dir="$(mktemp -d)"
+curl -L "$svg_archive" | tar --strip-components=1 -xz -C "$tmp_dir"
+
+input_dir="$tmp_dir/logos"
+svgo -f "$input_dir"
+
+# Make an array of all the .svg files
+svg_files=("$input_dir"/*.svg)
+total_files=${#svg_files[@]}
+
+processed_files=0
+
+# Iterate over every .svg file in the array
+for input_file in "${svg_files[@]}"; do
+    base_name=$(basename "$input_file" .svg)
+    output_file="$output_dir/$base_name.puml"
+
+    echo "@startuml" > "$output_file"
+    echo "sprite $base_name $(cat "$input_file")" >> "$output_file"
+    echo "@enduml" >> "$output_file"
+
+    percent=$((100 * $processed_files / $total_files))
+    echo "${percent}% Processed file: $base_name"
+    
+    processed_files=$((processed_files + 1))
+done
