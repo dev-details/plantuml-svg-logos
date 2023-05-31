2b8441b77892657a84482e46533af1d3a467e30a
Mike Thornton <361145+six8@users.noreply.github.com>
2023-05-31 14:43:37 -0500

    Add stdout option for preview script

--- .build/preview.sh
+++ .build/preview.sh
@@ -3,21 +3,24 @@
 set -euo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+test "${DEBUG:-}" && set -x
+
 PLANTUML_DIR="$(dirname "$DIR")/plantuml"
 
-# Check if the required dependencies are installed
-command -v plantuml >/dev/null 2>&1 || { echo >&2 "PlantUML is required but not installed. Aborting."; exit 1; }
+function command_exists {
+    type "$1" &> /dev/null
+}
 
-# Function to generate and display the PlantUML diagram
-function preview_plantuml() {
+function generate_plantuml() {
     local sprite_name="$1"
-    local output_file="$(mktemp -t plantuml_${sprite_name}).svg"
     local sprite_file="$PLANTUML_DIR/${sprite_name}.puml"
+    local sprite_contents=$(grep -v -e '@startuml' -e '@enduml' "$sprite_file")
 
-    # Generate the diagram using PlantUML
-    cat << EOF | plantuml -svg -pipe > "$output_file"
+    cat << EOF 
 @startuml
-!include ${sprite_file}
+${sprite_contents}
+
 rectangle "<\$${sprite_name}> <color:white>${sprite_name} black</color>" as black #000000
 rectangle "<\$${sprite_name}> ${sprite_name} gray" as gray #e6e6e6
 rectangle "<\$${sprite_name}> ${sprite_name} white" as white #ffffff
@@ -27,19 +30,52 @@ gray -down-> white
 
 @enduml
 EOF
+}
+
+function preview_plantuml() {
+    local sprite_name="$1"
+    local output_file="$(mktemp -t plantuml_${sprite_name}).svg"
+
+    # Generate the diagram using PlantUML
+    generate_plantuml "$sprite_name" | plantuml -svg -pipe > "$output_file"
 
     # Open the generated SVG in a web browser
     open "$output_file"
 }
 
-# Check if a sprite name was provided as an argument
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <sprite_name>"
-    exit 1
-fi
+function main {
+    local STDOUT=false
+    
+    while [[ $# -gt 0 ]]; do
+        key="$1"
+        case $key in
+            --stdout)
+            STDOUT=true
+            shift
+            ;;
+            *)
+            break
+            ;;
+        esac
+    done
 
-# Set the sprite name
-sprite_name="$1"
+    if [[ $# -ne 1 ]]; then
+        echo "Usage: $0 [--stdout] <sprite_name>"
+        exit 1
+    fi
 
-# Preview the PlantUML diagram
-preview_plantuml "$sprite_name"
+    local sprite_name="$1"
+
+    if $STDOUT ; then
+        generate_plantuml "$sprite_name"
+    else
+        if ! command_exists plantuml ; then
+            echo "PlantUML is not installed, run the command with --stdout option to output PlantUML contents."
+            exit 1
+        fi
+
+        preview_plantuml "$sprite_name"
+    fi
+}
+
+main "$@"
--- DEVELOPMENT.md
+++ DEVELOPMENT.md
@@ -1,5 +1,9 @@
 # Add a logo
 
+## Install SVGO
+
+[SVGO](https://github.com/svg/svgo) is required to optimize SVGs
+
 ## Find an SVG URL
 
 Searching for "{brandname} svg logo" in Google is usually enough.
@@ -17,6 +21,25 @@ Copy the URL to the SVG then run:
 
     .build/convert1.sh <sprite_name> <svg_url>
 
-# Preview logo
+This will generate a `plantuml/<sprite_name>.puml` file.
+
+## Preview logo
+
+If you have `plantuml` installed, you can generate a preview for the sprite:
 
     .build/preview.sh <sprite_name>
+
+If you do not have `plantuml` installed, you the following command to generate
+the PlantUML code and put it in your clipboard. You can then paste it into
+your IDE's PlantUML renderer (Online renderes like http://www.plantuml.com/plantuml/ do not support inline sprites).
+
+    .build/preview.sh --stdout <sprite_name> | pbcopy
+
+## Open a pull request
+
+Commit `plantuml/<sprite_name>.puml` with a message such as
+"Added <sprite_name> logo" and open a pull request to the `main` branch
+of https://github.com/dev-details/plantuml-svg-logos.
+
+In the PR description, paste the preview logo so reviewers can see how 
+it is rendered.
